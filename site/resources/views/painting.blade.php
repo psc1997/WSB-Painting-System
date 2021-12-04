1a9e26ed8a03bcbc3ed4911b32310dda6b3d0c2d
Damian Zabłocki <damizablocki99@gmail.com>
2021-12-04 19:05:45 +0100

    Routing do strony widoku obrazu

---
+++ site/resources/views/painting.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'PaintIT - Obraz')
+
+@section('content')
+    @include('painting.painting-categories')
+    @include('painting.painting-info')
+@endsection
