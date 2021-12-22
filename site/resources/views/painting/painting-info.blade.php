8bbb7a927cc54a5862dbd998c09f070d19c4d89b
szymczm01 <michalina.szymczak@icloud.com>
2021-12-22 18:42:59 +0100

    strona obrazu

--- site/resources/views/painting/painting-info.blade.php
+++ site/resources/views/painting/painting-info.blade.php
@@ -1,34 +1,35 @@
 <section class="painting-info">
     <div class="container">
         <div class="row">
-            <div class="col-24 col-md-12">
+            <div class="col-24 col-md-11">
                 <img src="https://placekitten.com/1000/1000" class="img-fluid painting-info__image" alt="">
             </div>
-            <div class="col-24 col-md-12">
+            <div class="col-24 col-md-11 offset-0 offset-md-2">
                 <div class="painting-info__description">
-                    <h5 class="painting-info__description-author">
-                        Adam
-                    </h5>
+                    <h2 class="painting-info__description-author">
+                        Adam Nowak
+                    </h2>
                     <p class="painting-info__description-likes">
-                        2102
+                        <span class="icon-heart-full painting-info__description-heart"></span> 2102
                     </p>
                     <h3 class="painting-info__description-title">
                         "Kotek"
                     </h3>
-                    <h5 class="painting-info__description-size">
-                        1000x1000
-                    </h5>
-                    <h5 class="painting-info__description-category">
-                        Zwierzęta
-                    </h5>
-                    <h5 class="painting-info__description-about">
-                        O autorze
-                    </h5>
-                    <p class="painting-info__description-about-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    <p class="painting-info__description-size">
+                        Akryl na płótnie 100x70
                     </p>
-                
-                </div>
+                    <p class="painting-info__description-category">
+                        Kategoria Zwierzęta
+                    </p>
+                    <div class="painting-info__description-about-author">
+                        <p class="painting-info__description-about">
+                        O Artyście
+                        </p>
+                        <p class="painting-info__description-about-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                    </div>
+                 </div>
             </div>
         </div>
     </div>
