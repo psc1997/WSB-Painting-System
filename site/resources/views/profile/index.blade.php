25faf4257622303bf96b768ff551d28333343c2c
Patryk Szulc <psc1997@o2.pl>
2022-01-06 17:53:43 +0100

    Profil użytkownika (71%)

--- site/resources/views/profile/index.blade.php
+++ site/resources/views/profile/index.blade.php
@@ -7,13 +7,21 @@
                 <div class="col-24 col-md-6">
                     <img src="{{ url('/') . $user_images['avatar'] }}" alt="Avatar {{ $username ?? '' }}" class="img profile-header__avatar">
                 </div>
-                <div class="col-24 col-md-17 offset-0 offset-md-1">
+                <div class="col-24 col-md-14 offset-0 offset-md-1">
                     <p class="profile-header__pretitle">
                         Użytkownik:
                     </p>
                     <h1 class="profile-header__title">
                         {{ $username ?? '' }}
                     </h1>
+                </div>
+                <div class="col-24 col-md-3 d-flex justify-content-end">
+                    <a href="{{ route('profile.edit', Auth::user()->name) }}" class="profile-header__button-edit">
+                        <div>
+                            <span class="icon icon-user-edit"></span><br>
+                            Edytuj
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
